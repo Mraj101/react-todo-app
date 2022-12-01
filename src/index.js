f5fcb856f6d5e5734bca86f21b60222d430099e9
Hasnain <mrajhasnain101@gmail.com>
2022-12-01 23:17:08 +0600

    centered container's of home.module.css

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <div className='container'>
+
+        <App />
+    </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
