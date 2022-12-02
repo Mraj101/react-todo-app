c3c05808613ac230537768d37d03be9eb607f9be
Hasnain <mrajhasnain101@gmail.com>
2022-12-02 16:45:56 +0600

    more css

--- src/components/NewTodo.js
+++ src/components/NewTodo.js
@@ -1,10 +1,21 @@
 import React from 'react'
 
+import style from './form.module.css'
 const NewTodo = () => {
   return (
     <div>
-        <label htmlFor="title">Title:</label>
-        <input type="text" name='title' id='title'  />
+      <form className={style["form-container"]}>
+        <div className={style["form-group"]}>
+            <label htmlFor="title">Enter Title:</label>
+            <input type="text" name='title' id='title'  />
+        </div>
+
+        <div className={style["form-group"]} > 
+            <label htmlFor="description">Description: </label>
+            <input type="text" id='description' name='description' />
+        </div>
+  
+      </form>
     </div>
   )
 }
